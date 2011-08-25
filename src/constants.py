ce390b251d0ad3850a088bab0e704481e6821c4d
Tomás Touceda <chiiph@torproject.org>
2011-08-25 17:04:38 -0300

    Cambiar la definicion de los tokens

--- src/constants.py
+++ src/constants.py
@@ -1,46 +1,62 @@
-IDENTIFIER = 0
-SEPARATOR = 1
-INT_LITERAL = 2
-CHAR_LITERAL = 3
-STRING_LITERAL = 4
-SCOLON = 5
-BRACE_OPEN = 6
-BRACE_CLOSE = 7
-PAREN_OPEN = 8
-PAREN_CLOSE = 9
-CLASS = 10
-EXTENDS = 11
-PUBLIC = 12
-PROTECTED = 13
-STATIC = 14
-THIS = 15
-SUPER = 16
-VOID_TYPE = 17
-BOOLEAN_TYPE = 18
-INT_TYPE = 19
-CHAR_TYPE = 20
-IF = 21
-THE = 22
-ELSE = 23
-WHILE = 24
-RETURN = 25
-TRUE = 26
-FALSE = 27
-NULL = 28
-NEW = 29
-ASSIGNMENT = 30
-CONDITIONAL_AND = 31
-EQUALS = 32
-NOT_EQUALS = 33
-LT = 34
-GT = 35
-LT_EQ = 36
-GT_EQ = 37
-ADD = 38
-SUB = 39
-MUL = 40
-DIV = 41
-MOD = 42
-NOT = 43
-ACCESSOR = 44
-EOF = 45
+class TokenType(object):
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
+
+    def __eq__(self, other):
+        return self._id == other._id
+
+    def __str__(self):
+        return self._name
+
+    def __repr__(self):
+        return "TokenType(%d,%s)" % (self._id, self._name)
+
+TT = TokenType
+
+IDENTIFIER = TT(0, "<IDENTIFIER>")
+SEPARATOR = TT(1, "<SEPARATOR>")
+INT_LITERAL = TT(2, "<INT_LITERAL>")
+CHAR_LITERAL = TT(0, "<CHAR_LITERAL>")
+STRING_LITERAL = TT(4, "<STRING_LITERAL>")
+SCOLON = TT(5, "<SCOLON>")
+BRACE_OPEN = TT(6, "<BRACE_OPEN>")
+BRACE_CLOSE = TT(7, "<BRACE_CLOSE>")
+PAREN_OPEN = TT(8, "<PAREN_OPEN>")
+PAREN_CLOSE = TT(9, "<PAREN_CLOSE>")
+CLASS = TT(10, "<CLASS>")
+EXTENDS = TT(11, "<EXTENDS>")
+PUBLIC = TT(12, "<PUBLIC>")
+PROTECTED = TT(13, "<PROTECTED>")
+STATIC = TT(14, "<STATIC>")
+THIS = TT(15, "<THIS>")
+SUPER = TT(16, "<SUPER>")
+VOID_TYPE = TT(17, "<VOID_TYPE>")
+BOOLEAN_TYPE = TT(18, "<BOOLEAN_TYPE>")
+INT_TYPE = TT(19, "<INT_TYPE>")
+CHAR_TYPE = TT(20, "<CHAR_TYPE>")
+IF = TT(21, "<IF>")
+THEN = TT(22, "<THEN>")
+ELSE = TT(23, "<ELSE>")
+WHILE = TT(24, "<WHILE>")
+RETURN = TT(25, "<RETURN>")
+TRUE = TT(26, "<TRUE>")
+FALSE = TT(27, "<FALSE>")
+NULL = TT(28, "<NULL>")
+NEW = TT(29, "<NEW>")
+ASSIGNMENT = TT(30, "<ASSIGNMENT>")
+CONDITIONAL_AND = TT(31, "<CONDITIONAL_AND>")
+EQUALS = TT(32, "<EQUALS>")
+NOT_EQUALS = TT(33, "<NOT_EQUALS>")
+LT = TT(34, "<LT>")
+GT = TT(35, "<GT>")
+LT_EQ = TT(36, "<LT_EQ>")
+GT_EQ = TT(37, "<GT_EQ>")
+ADD = TT(38, "<ADD>")
+SUB = TT(39, "<SUB>")
+MUL = TT(40, "<MUL>")
+DIV = TT(41, "<DIV>")
+MOD = TT(42, "<MOD>")
+NOT = TT(43, "<NOT>")
+ACCESSOR = TT(44, "<ACCESSOR>")
+EOF = TT(45, "<EOF>")
