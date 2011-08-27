2a68eb812b5ed5418de6a582923dac721774ae20
Tomás Touceda <chiiph@torproject.org>
2011-08-27 10:38:00 -0300

    Remueve comentarios como preprocesado y los reemplaza por un separador

--- src/lexor.py
+++ src/lexor.py
@@ -1,7 +1,7 @@
 from constants import *
 from states import *
 
-import os
+import os, re
 
 class Token(object):
     def __init__(self):
@@ -40,11 +40,19 @@ class Lexor(object):
         self._current_char  = ""
         self._file_path = file_path
         self._file = open(self._file_path, "r")
+        self._cursor = -1
+        self._file_data = self._file.read()
         self._one_sep = False
         self._maybe_start_comment = False
 
+        self._remove_comments()
+
     def __del__(self):
         self._file.close()
+
+    def _remove_comments(self):
+        tmp = re.sub("//.*?\n|/\*.*?\*/", " ", self._file_data, re.S)
+        self._file_data = tmp
 
     def get_token(self):
         self._state = ST_INITIAL
@@ -76,8 +84,14 @@ class Lexor(object):
             self._current_token._type = reserved_words[self._current_token.get_lexeme()]
         return self._current_token
 
+    def _real_next_char(self):
+        self._cursor += 1
+        if self._cursor >= len(self._file_data):
+            return ""
+        return self._file_data[self._cursor]
+
     def _next_char(self):
-        ch = self._file.read(1)
+        ch = self._real_next_char()
         self._col += 1
 
         if ch in self._whitespace and not self._one_sep:
@@ -91,43 +105,9 @@ class Lexor(object):
             if ch == "\n":
                 self._line += 1
                 self._col = 0
-            ch = self._file.read(1)
+            ch = self._real_next_char()
             self._col += 1
 
         self._one_sep = False
 
-        ch2 = self._file.read(1)
-
-        if ch == "/" and ch2 == "/":
-            print "DEFINITELY A COMMENT"
-            while ch != "\n":
-                ch = self._file.read(1)
-                if len(ch) == 0:
-                    return ch
-            self._line += 1
-            self._col = 0
-            ch = self._next_char()
-        elif ch == "/" and ch2 == "*":
-            init_col = self._col
-            init_line = self._line
-            ch = self._file.read(1)
-            self._col += 1
-            while True:
-                while ch != "*":
-                    ch = self._next_char()
-                    if len(ch) == 0:
-                        raise LexicalError(self._line, self._col,
-                                           "Comentario no cerrado :: Comienza en la linea %d, columna %d" % (init_line,
-                                                                                                             init_col))
-                ch = self._file.read(1)
-                self._col += 1
-                if ch == "/":
-                    break
-            ch = self._next_char()
-        else:
-            if len(ch2) > 0:
-                self._file.seek(-1, os.SEEK_CUR)
-
-        if self._one_sep:
-            ch = self._next_char()
         return ch
