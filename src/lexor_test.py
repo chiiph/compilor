f3f26f26d18f83a1001e85fc1655de78098bf867
Iñaki Garay <igarai@gmail.com>
2011-08-27 10:45:07 -0300

    Completado el modulo principal.

--- src/lexor_test.py
+++ src/lexor_test.py
@@ -1,16 +1,45 @@
-from lexor import *
+import sys
+from lexor     import *
 from constants import *
-from errors import LexicalError
+from errors    import LexicalError
+
+def usage():
+    usage_str = """
+    Error: two arguments required.
+    Usage:
+        lexor INPUTFILE OUTPUTFILE
+
+    """
+    print usage_str
 
 if __name__ == "__main__":
-    lex = Lexor("test.java")
-    while True:
+    argv_len = len(sys.argv)
+    if ((argv_len == 3) or (argv_len == 2)):
+        input_filepath  = sys.argv[1]
         try:
-            tok = lex.get_token()
-            if tok.get_type() == EOF:
-                break
-        except LexicalError as e:
-            print "\n", e
+            lex = Lexor(input_filepath)
+        except IOError as ioe:
+            print "Error: no such file."
             quit()
+        
+        if (argv_len == 3):
+            output_filepath = sys.argv[2]
+            output_file = open(output_filepath, 'w')
+            sys.stdout = output_file
 
-        print tok
+        while True:
+            try:
+                tok = lex.get_token()
+                if tok.get_type() == EOF:
+                    break
+            except LexicalError as e:
+                print "\n", e
+                quit()
+            print tok
+        
+        if (argv_len == 3):
+            output_file.close()
+    else:
+        usage()
+
+
