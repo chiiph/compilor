40d7d4f1d46fc0ad135dca4ec3dcbf18d0752ea4
Tomás Touceda <chiiph@torproject.org>
2011-08-28 19:32:50 -0300

    Agrega los archivos restantes

---
+++ tests/lexor/000cor.java
@@ -0,0 +1,23 @@
+class Test {
+    private int numero;
+    private bool booleano;
+
+    public static void main() {
+        int /* s */  a,b;
+
+        a = 1;
+        b=2 ;numero = 42;
+
+        if(a!= b)
+            numero = 4;;
+
+        while(numero <= 2) {
+            booleando = b == a/*a*/-2;
+            numero /** comentario en el medio */ = numero - 1;
+        }
+    }
+
+    private char private_method() {
+        return 'a';// a verrr
+    }
+}
