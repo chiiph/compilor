0fd180a91a4ad3aec6074fae14ab0000270713cf
Iñaki Garay <igarai@gmail.com>
2011-08-29 23:28:35 -0300

    Indentacion.

--- src/constants.py
+++ src/constants.py
@@ -14,70 +14,71 @@ class TokenType(object):
 
 TT = TokenType
 
-IDENTIFIER = TT(0, "<IDENTIFIER>")
-SEPARATOR = TT(1, "<SEPARATOR>")
-INT_LITERAL = TT(2, "<INT_LITERAL>")
-CHAR_LITERAL = TT(0, "<CHAR_LITERAL>")
-STRING_LITERAL = TT(4, "<STRING_LITERAL>")
-SCOLON = TT(5, "<SCOLON>")
-BRACE_OPEN = TT(6, "<BRACE_OPEN>")
-BRACE_CLOSE = TT(7, "<BRACE_CLOSE>")
-PAREN_OPEN = TT(8, "<PAREN_OPEN>")
-PAREN_CLOSE = TT(9, "<PAREN_CLOSE>")
-CLASS = TT(10, "<CLASS>")
-EXTENDS = TT(11, "<EXTENDS>")
-PUBLIC = TT(12, "<PUBLIC>")
-PROTECTED = TT(13, "<PROTECTED>")
-STATIC = TT(14, "<STATIC>")
-THIS = TT(15, "<THIS>")
-SUPER = TT(16, "<SUPER>")
-VOID_TYPE = TT(17, "<VOID_TYPE>")
-BOOLEAN_TYPE = TT(18, "<BOOLEAN_TYPE>")
-INT_TYPE = TT(19, "<INT_TYPE>")
-CHAR_TYPE = TT(20, "<CHAR_TYPE>")
-IF = TT(21, "<IF>")
-THEN = TT(22, "<THEN>")
-ELSE = TT(23, "<ELSE>")
-WHILE = TT(24, "<WHILE>")
-RETURN = TT(25, "<RETURN>")
-TRUE = TT(26, "<TRUE>")
-FALSE = TT(27, "<FALSE>")
-NULL = TT(28, "<NULL>")
-NEW = TT(29, "<NEW>")
-ASSIGNMENT = TT(30, "<ASSIGNMENT>")
+IDENTIFIER      = TT( 0, "<IDENTIFIER>")
+SEPARATOR       = TT( 1, "<SEPARATOR>")
+INT_LITERAL     = TT( 2, "<INT_LITERAL>")
+CHAR_LITERAL    = TT( 0, "<CHAR_LITERAL>")
+STRING_LITERAL  = TT( 4, "<STRING_LITERAL>")
+SCOLON          = TT( 5, "<SCOLON>")
+BRACE_OPEN      = TT( 6, "<BRACE_OPEN>")
+BRACE_CLOSE     = TT( 7, "<BRACE_CLOSE>")
+PAREN_OPEN      = TT( 8, "<PAREN_OPEN>")
+PAREN_CLOSE     = TT( 9, "<PAREN_CLOSE>")
+CLASS           = TT(10, "<CLASS>")
+EXTENDS         = TT(11, "<EXTENDS>")
+PUBLIC          = TT(12, "<PUBLIC>")
+PROTECTED       = TT(13, "<PROTECTED>")
+STATIC          = TT(14, "<STATIC>")
+THIS            = TT(15, "<THIS>")
+SUPER           = TT(16, "<SUPER>")
+VOID_TYPE       = TT(17, "<VOID_TYPE>")
+BOOLEAN_TYPE    = TT(18, "<BOOLEAN_TYPE>")
+INT_TYPE        = TT(19, "<INT_TYPE>")
+CHAR_TYPE       = TT(20, "<CHAR_TYPE>")
+IF              = TT(21, "<IF>")
+THEN            = TT(22, "<THEN>")
+ELSE            = TT(23, "<ELSE>")
+WHILE           = TT(24, "<WHILE>")
+RETURN          = TT(25, "<RETURN>")
+TRUE            = TT(26, "<TRUE>")
+FALSE           = TT(27, "<FALSE>")
+NULL            = TT(28, "<NULL>")
+NEW             = TT(29, "<NEW>")
+ASSIGNMENT      = TT(30, "<ASSIGNMENT>")
 CONDITIONAL_AND = TT(31, "<CONDITIONAL_AND>")
-EQUALS = TT(32, "<EQUALS>")
-NOT_EQUALS = TT(33, "<NOT_EQUALS>")
-LT = TT(34, "<LT>")
-GT = TT(35, "<GT>")
-LT_EQ = TT(36, "<LT_EQ>")
-GT_EQ = TT(37, "<GT_EQ>")
-ADD = TT(38, "<ADD>")
-SUB = TT(39, "<SUB>")
-MUL = TT(40, "<MUL>")
-DIV = TT(41, "<DIV>")
-MOD = TT(42, "<MOD>")
-NOT = TT(43, "<NOT>")
-ACCESSOR = TT(44, "<ACCESSOR>")
-EOF = TT(45, "<EOF>")
+EQUALS          = TT(32, "<EQUALS>")
+NOT_EQUALS      = TT(33, "<NOT_EQUALS>")
+LT              = TT(34, "<LT>")
+GT              = TT(35, "<GT>")
+LT_EQ           = TT(36, "<LT_EQ>")
+GT_EQ           = TT(37, "<GT_EQ>")
+ADD             = TT(38, "<ADD>")
+SUB             = TT(39, "<SUB>")
+MUL             = TT(40, "<MUL>")
+DIV             = TT(41, "<DIV>")
+MOD             = TT(42, "<MOD>")
+NOT             = TT(43, "<NOT>")
+ACCESSOR        = TT(44, "<ACCESSOR>")
+EOF             = TT(45, "<EOF>")
 
-reserved_words = {"class" : CLASS,
-                  "extends" : EXTENDS,
-                  "public" : PUBLIC,
-                  "protected" : PROTECTED,
-                  "static" : STATIC,
-                  "this" : THIS,
-                  "super" : SUPER,
-                  "void" : VOID_TYPE,
-                  "bool" : BOOLEAN_TYPE,
-                  "int" : INT_TYPE,
-                  "char" : CHAR_TYPE,
-                  "if" : IF,
-                  "then" : THEN,
-                  "else" : ELSE,
-                  "while" : WHILE,
-                  "return" : RETURN,
-                  "true" : TRUE,
-                  "false" : FALSE,
-                  "null" : NULL,
-                  "new" : NEW }
+reserved_words = { "class"      : CLASS,
+                 , "extends"    : EXTENDS
+                 , "public"     : PUBLIC
+                 , "protected"  : PROTECTED
+                 , "static"     : STATIC
+                 , "this"       : THIS
+                 , "super"      : SUPER
+                 , "void"       : VOID_TYPE
+                 , "bool"       : BOOLEAN_TYPE
+                 , "int"        : INT_TYPE
+                 , "char"       : CHAR_TYPE
+                 , "if"         : IF
+                 , "then"       : THEN
+                 , "else"       : ELSE
+                 , "while"      : WHILE
+                 , "return"     : RETURN
+                 , "true"       : TRUE
+                 , "false"      : FALSE
+                 , "null"       : NULL
+                 , "new"        : NEW
+                 }
--- src/states.py
+++ src/states.py
@@ -41,115 +41,116 @@ class State(object):
     def get_token_type(self):
         return self._token_type
 
-_check_IDENTIFIER = lambda c: c in string.ascii_letters+"_$"
-_check_rest_IDENTIFIER = lambda c: c in string.ascii_letters+string.digits+"_$"
-ST_IDENTIFIER = State([(None, True, _check_rest_IDENTIFIER)],
-                       True, IDENTIFIER)
+_check_IDENTIFIER           = lambda c: c in string.ascii_letters+"_$"
+_check_rest_IDENTIFIER      = lambda c: c in string.ascii_letters+string.digits+"_$"
+ST_IDENTIFIER               = State([(None, True, _check_rest_IDENTIFIER)], True, IDENTIFIER)
 
-_check_ZERO = lambda c: c == "0"
-ST_ZERO_LITERAL = State([], True, INT_LITERAL)
+_check_ZERO                 = lambda c: c == "0"
+ST_ZERO_LITERAL             = State([], True, INT_LITERAL)
 
-_check_INT = lambda c: c in string.digits[1:]
-_check_rest_INT = lambda c: c in string.digits
-ST_INT_LITERAL = State([(None, True, _check_rest_INT)],
-                       True, INT_LITERAL)
+_check_INT                  = lambda c: c in string.digits[1:]
+_check_rest_INT             = lambda c: c in string.digits
+ST_INT_LITERAL              = State([(None, True, _check_rest_INT)], True, INT_LITERAL)
 
-_check_SEPARATOR = lambda c: c in [" ",","]
-ST_SEPARATOR = State([], True, SEPARATOR)
+_check_SEPARATOR            = lambda c: c in [" ",","]
+ST_SEPARATOR                = State([], True, SEPARATOR)
 
-_check_CHAR_QUOTE = lambda c: c == "\'"
-_check_CHAR = lambda c: (c in string.printable and c != "\\" and c != "\'")
-_check_ESCAPED_CHAR_START = lambda c: c == "\\"
-_check_ESCAPED_CHAR_END = lambda c: c in "\\\'\"n"
-ST_CHAR_QUOTE_END = State([], True, CHAR_LITERAL)
-ST_CHAR_END = State([(ST_CHAR_QUOTE_END, False, _check_CHAR_QUOTE)], False, None)
-ST_ESCAPED_CHAR = State([(ST_CHAR_END, False, _check_ESCAPED_CHAR_END)], False, None)
-ST_CHAR_QUOTE = State([(ST_CHAR_END, False, _check_CHAR),
-                       (ST_ESCAPED_CHAR, False, _check_ESCAPED_CHAR_START)], False, None)
+_check_CHAR_QUOTE           = lambda c: c == "\'"
+_check_CHAR                 = lambda c: (c in string.printable and c != "\\" and c != "\'")
+_check_ESCAPED_CHAR_START   = lambda c: c == "\\"
+_check_ESCAPED_CHAR_END     = lambda c: c in "\\\'\"n"
+ST_CHAR_QUOTE_END           = State([], True, CHAR_LITERAL)
+ST_CHAR_END                 = State([(ST_CHAR_QUOTE_END, False, _check_CHAR_QUOTE)], False, None)
+ST_ESCAPED_CHAR             = State([(ST_CHAR_END,      False, _check_ESCAPED_CHAR_END)], False, None)
+ST_CHAR_QUOTE               = State([ (ST_CHAR_END,     False, _check_CHAR)
+                                    , (ST_ESCAPED_CHAR, False, _check_ESCAPED_CHAR_START)
+                                    ], False, None)
 
-_check_STRING_QUOTE = lambda c: c == "\""
-_check_STRING_CHAR = lambda c: (c in string.printable and c != "\\" and c != "\"")
-ST_STRING_END = State([], True, STRING_LITERAL)
-ST_STRING_START = State([], False, None) # Hack
-ST_ESCAPED_CHAR = State([(ST_STRING_START, False, _check_ESCAPED_CHAR_END)], False, None)
-ST_STRING_START._next_state = [(None, True, _check_STRING_CHAR),
-                               (ST_ESCAPED_CHAR, False, _check_ESCAPED_CHAR_START),
-                               (ST_STRING_END, False, _check_STRING_QUOTE)]
+_check_STRING_QUOTE         = lambda c: c == "\""
+_check_STRING_CHAR          = lambda c: (c in string.printable and c != "\\" and c != "\"")
+ST_STRING_END               = State([], True, STRING_LITERAL)
+ST_STRING_START             = State([], False, None) # Hack
+ST_ESCAPED_CHAR             = State([(ST_STRING_START, False, _check_ESCAPED_CHAR_END)], False, None)
+ST_STRING_START._next_state = [ (None, True, _check_STRING_CHAR)
+                              , (ST_ESCAPED_CHAR, False, _check_ESCAPED_CHAR_START)
+                              , (ST_STRING_END, False, _check_STRING_QUOTE)
+                              ]
 
-_check_EQUALS = lambda c: c == "="
-ST_EQUALS = State([], True, EQUALS)
-ST_ASSIGNMENT = State([(ST_EQUALS, False, _check_EQUALS)], True, ASSIGNMENT)
+_check_EQUALS               = lambda c: c == "="
+ST_EQUALS                   = State([], True, EQUALS)
+ST_ASSIGNMENT               = State([(ST_EQUALS, False, _check_EQUALS)], True, ASSIGNMENT)
 
-_check_SCOLON = lambda c: c == ";"
-ST_SCOLON = State([], True, SCOLON)
+_check_SCOLON               = lambda c: c == ";"
+ST_SCOLON                   = State([], True, SCOLON)
 
-_check_BRACE_OPEN = lambda c: c == "{"
-ST_BRACE_OPEN = State([], True, BRACE_OPEN)
+_check_BRACE_OPEN           = lambda c: c == "{"
+ST_BRACE_OPEN               = State([], True, BRACE_OPEN)
 
-_check_BRACE_CLOSE = lambda c: c == "}"
-ST_BRACE_CLOSE = State([], True, BRACE_CLOSE)
+_check_BRACE_CLOSE          = lambda c: c == "}"
+ST_BRACE_CLOSE              = State([], True, BRACE_CLOSE)
 
-_check_PAREN_OPEN = lambda c: c == "("
-ST_PAREN_OPEN = State([], True, PAREN_OPEN)
+_check_PAREN_OPEN           = lambda c: c == "("
+ST_PAREN_OPEN               = State([], True, PAREN_OPEN)
 
-_check_PAREN_CLOSE = lambda c: c == ")"
-ST_PAREN_CLOSE = State([], True, PAREN_CLOSE)
+_check_PAREN_CLOSE          = lambda c: c == ")"
+ST_PAREN_CLOSE              = State([], True, PAREN_CLOSE)
 
-_check_LT = lambda c: c == "<"
-ST_LT_EQ = State([], True, LT_EQ)
-ST_LT = State([(ST_LT_EQ, False, _check_EQUALS)], True, LT)
+_check_LT                   = lambda c: c == "<"
+ST_LT_EQ                    = State([], True, LT_EQ)
+ST_LT                       = State([(ST_LT_EQ, False, _check_EQUALS)], True, LT)
 
-_check_GT = lambda c: c == ">"
-ST_GT_EQ = State([], True, GT_EQ)
-ST_GT = State([(ST_GT_EQ, False, _check_EQUALS)], True, GT)
+_check_GT                   = lambda c: c == ">"
+ST_GT_EQ                    = State([], True, GT_EQ)
+ST_GT                       = State([(ST_GT_EQ, False, _check_EQUALS)], True, GT)
 
-_check_ADD = lambda c: c == "+"
-ST_ADD = State([], True, ADD)
+_check_ADD                  = lambda c: c == "+"
+ST_ADD                      = State([], True, ADD)
 
-_check_SUB = lambda c: c == "-"
-ST_SUB = State([], True, SUB)
+_check_SUB                  = lambda c: c == "-"
+ST_SUB                      = State([], True, SUB)
 
-_check_MUL = lambda c: c == "*"
-ST_MUL = State([], True, MUL)
+_check_MUL                  = lambda c: c == "*"
+ST_MUL                      = State([], True, MUL)
 
-_check_DIV = lambda c: c == "/"
-ST_DIV = State([], True, DIV)
+_check_DIV                  = lambda c: c == "/"
+ST_DIV                      = State([], True, DIV)
 
-_check_MOD = lambda c: c == "%"
-ST_MOD = State([], True, MOD)
+_check_MOD                  = lambda c: c == "%"
+ST_MOD                      = State([], True, MOD)
 
-_check_NOT = lambda c: c == "!"
-ST_NOT_EQUALS = State([], True, NOT_EQUALS)
-ST_NOT = State([(ST_NOT_EQUALS, False, _check_EQUALS)], True, NOT)
+_check_NOT                  = lambda c: c == "!"
+ST_NOT_EQUALS               = State([], True, NOT_EQUALS)
+ST_NOT                      = State([(ST_NOT_EQUALS, False, _check_EQUALS)], True, NOT)
 
-_check_ACCESSOR = lambda c: c == "."
-ST_ACCESSOR = State([], True, ACCESSOR)
+_check_ACCESSOR             = lambda c: c == "."
+ST_ACCESSOR                 = State([], True, ACCESSOR)
 
-_check_AMP = lambda c: c == "&"
-ST_CONDITIONAL_AND_END = State([], True, CONDITIONAL_AND)
-ST_CONDITIONAL_AND = State([(ST_CONDITIONAL_AND_END, False, _check_AMP)], False, None)
+_check_AMP                  = lambda c: c == "&"
+ST_CONDITIONAL_AND_END      = State([], True, CONDITIONAL_AND)
+ST_CONDITIONAL_AND          = State([(ST_CONDITIONAL_AND_END, False, _check_AMP)], False, None)
 
-
-ST_INITIAL = State([(ST_IDENTIFIER, False, _check_IDENTIFIER),
-                    (ST_ZERO_LITERAL, False, _check_ZERO),
-                    (ST_SEPARATOR, False, _check_SEPARATOR),
-                    (ST_INT_LITERAL, False, _check_INT),
-                    (ST_ASSIGNMENT, False, _check_EQUALS),
-                    (ST_CHAR_QUOTE, False, _check_CHAR_QUOTE),
-                    (ST_STRING_START, False, _check_STRING_QUOTE),
-                    (ST_SCOLON, False, _check_SCOLON),
-                    (ST_BRACE_OPEN, False, _check_BRACE_OPEN),
-                    (ST_BRACE_CLOSE, False, _check_BRACE_CLOSE),
-                    (ST_PAREN_OPEN, False, _check_PAREN_OPEN),
-                    (ST_PAREN_CLOSE, False, _check_PAREN_CLOSE),
-                    (ST_LT, False, _check_LT),
-                    (ST_GT, False, _check_GT),
-                    (ST_ADD, False, _check_ADD),
-                    (ST_SUB, False, _check_SUB),
-                    (ST_MUL, False, _check_MUL),
-                    (ST_DIV, False, _check_DIV),
-                    (ST_MOD, False, _check_MOD),
-                    (ST_NOT, False, _check_NOT),
-                    (ST_ACCESSOR, False, _check_ACCESSOR),
-                    (ST_CONDITIONAL_AND, False, _check_AMP)],
-                    False, None)
+ST_INITIAL                  = State([ (ST_IDENTIFIER,       False, _check_IDENTIFIER)
+                                    , (ST_ZERO_LITERAL,     False, _check_ZERO)
+                                    , (ST_SEPARATOR,        False, _check_SEPARATOR)
+                                    , (ST_INT_LITERAL,      False, _check_INT)
+                                    , (ST_ASSIGNMENT,       False, _check_EQUALS)
+                                    , (ST_CHAR_QUOTE,       False, _check_CHAR_QUOTE)
+                                    , (ST_STRING_START,     False, _check_STRING_QUOTE)
+                                    , (ST_SCOLON,           False, _check_SCOLON)
+                                    , (ST_BRACE_OPEN,       False, _check_BRACE_OPEN)
+                                    , (ST_BRACE_CLOSE,      False, _check_BRACE_CLOSE)
+                                    , (ST_PAREN_OPEN,       False, _check_PAREN_OPEN)
+                                    , (ST_PAREN_CLOSE,      False, _check_PAREN_CLOSE)
+                                    , (ST_LT,               False, _check_LT)
+                                    , (ST_GT,               False, _check_GT)
+                                    , (ST_ADD,              False, _check_ADD)
+                                    , (ST_SUB,              False, _check_SUB)
+                                    , (ST_MUL,              False, _check_MUL)
+                                    , (ST_DIV,              False, _check_DIV)
+                                    , (ST_MOD,              False, _check_MOD)
+                                    , (ST_NOT,              False, _check_NOT)
+                                    , (ST_ACCES SOR,        False, _check_ACCESSOR)
+                                    , (ST_CONDITIONAL_AND,  False, _check_AMP)
+                                    ],
+                                    False, 
+                                    None)
