1437dfa97b1582cdae1807bf9c08601db156c55d
Iñaki Garay <igarai@gmail.com>
2011-09-08 22:04:13 -0300

    Started correct test suite.

---
+++ tests/syntaxor/000cor.java
@@ -0,0 +1,92 @@
+public class ClassIdentifier1 extends ClassIdentifier2
+{
+    public int              variableIndentifier01;
+    public char             variableIndentifier02;
+    public boolean          variableIndentifier03;
+    public ClassIdentifier3 variableIndentifier04;
+
+    private int              variableIndentifier05;
+    private char             variableIndentifier06;
+    private boolean          variableIndentifier07;
+    private ClassIdentifier3 variableIndentifier08;
+
+    protected int              variableIndentifier09;
+    protected char             variableIndentifier10;
+    protected boolean          variableIndentifier11;
+    protected ClassIdentifier3 variableIndentifier12;
+
+    public    ClassIdentifier1 ( int              variableIdentifier1
+                               , char             variableIdentifier2
+                               , boolean          variableIdentifier3
+                               , ClassIdentifier3 variableIdentifier4
+                               )
+    {}
+
+    protected ClassIdentifier1 ( int              variableIdentifier1
+                               , char             variableIdentifier2
+                               , boolean          variableIdentifier3
+                               , ClassIdentifier3 variableIdentifier4
+                               )
+    {}
+
+    public void methodIdentifier() 
+    {}
+
+    public int methodIdentifier() 
+    {}
+
+    public char methodIdentifier() 
+    {}
+
+    public boolean methodIdentifier() 
+    {}
+
+    public ClassIdentifier2 methodIdentifier() 
+    {}
+
+    public static void methodIdentifier() 
+    {}
+
+    public static int methodIdentifier() 
+    {}
+
+    public static char methodIdentifier() 
+    {}
+
+    public static boolean methodIdentifier() 
+    {}
+
+    public static ClassIdentifier2 methodIdentifier() 
+    {}
+
+    private void methodIdentifier() 
+    {}
+
+    private int methodIdentifier() 
+    {}
+
+    private char methodIdentifier() 
+    {}
+
+    private boolean methodIdentifier() 
+    {}
+
+    private ClassIdentifier2 methodIdentifier() 
+    {}
+
+    private static void methodIdentifier() 
+    {}
+
+    private static int methodIdentifier() 
+    {}
+
+    private static char methodIdentifier() 
+    {}
+
+    private static boolean methodIdentifier() 
+    {}
+
+    private static ClassIdentifier2 methodIdentifier() 
+    {}
+
+}
