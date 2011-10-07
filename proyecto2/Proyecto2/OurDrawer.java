25df5bb60489d099543fdb7e68a2b0c981889a52
Iñaki Garay <igarai@gmail.com>
2011-10-07 18:42:37 -0300

    Added OurDrawer.

--- proyecto2/Proyecto2/OurDrawer.java
+++ proyecto2/Proyecto2/OurDrawer.java
@@ -4,5 +4,34 @@ public class OurDrawer {
 
     private drawer;
 
+    public OurDrawer() {
+        drawer = new Drawer();
+    }
+
+    public void eval(String command) {
+        
+        String[] command_parameters = command.split(",");
+
+        if (command.startsWith("draw")) {
+            drawer.draw(Integer(command_parameters[1]));
+        }
+        if (command.startsWith("draw_named")) {
+            drawer.drawNameDraw(Integer(command_parameters[1]));
+        }
+        if (command.startsWith("rotate")) {
+            drawer.rotate(Integer(command_parameters[1]));
+        }
+        if (command.startsWith("move")) {
+            drawer.move(Integer(command_parameters[1]));
+        }
+    }
+
+    public void reset() {
+        drawer.reset();
+    }
+
+    public void setColor(int r, int g, int b) {
+        drawer.seColor(r, g, b);
+    }
 
 }
